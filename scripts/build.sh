66960201dd466e775b9ceb74273bfd797d59ca9b
duanzhengmou <duanzhengmou@bytedance.com>
2025-08-13 08:23:59 +0800

    chore: change build target

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,11 @@ cd frontend
 pnpm install
 pnpm run build
 
+cd ../backend
+# go build -o output/server backend/main.go
+
+# 默认构建 Linux
+GOOS=linux GOARCH=amd64 go build -o ../output/server ./main.go
+
 cd ..
-go build -o output/server backend/main.go
 cp -r frontend/dist output/
